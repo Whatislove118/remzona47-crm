0b14c1794723bab7c44255eacf929363a9fc379d
whatislove118 <oleg.pochikalin@mail.ru>
2022-04-24 18:45:48 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-from core.startup import create_base_groups
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
+
+from core.startup import create_base_groups
 create_base_groups()
